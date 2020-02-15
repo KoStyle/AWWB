3695b1b546b5ebc4626dbbc2cd5f2cfcf6d04eda
konomi_casa <konomi@360@gmail.com>
2020-02-15 01:42:32 +0100

    Modifications to Assasination Addition of Coffee event (unfinished, missing full bang)

--- venv/events.py
+++ venv/events.py
@@ -1,36 +1,60 @@
+import util
+
+
 class Assasination:
-    def __init__(self):
-        self.frecuency=0.0
-        self.lastTweet=""
-        self.curretTweet=""
-        self.harpies=None
-
-    def __init__(self, frecuency):
-        self.frecuency=frecuency
-        self.lastTweet=""
-        self.curretTweet=""
-        self.harpies=None
-
     def __init__(self, frecuency, harpies):
-        self.frecuency=frecuency
-        self.lastTweet=""
-        self.curretTweet=""
-        self.harpies=harpies
+        self.frecuency = frecuency
+        self.lastTweet = ""
+        self.curretTweet = ""
+        self.harpies = harpies
+
 
     def bang(self):
+        return "Killer Tweet"
 
-
-
-        return "Tweet"
 
     def chooseKiller(self, tmpHarpies):
         threshold = random.random()
         random.shuffle(tmpHarpies)
+
         pot = tmpHarpies[0].percKill
         i = 0
         while pot < threshold:
             pot = pot + tmpHarpies[i].percKill
-            ++i
+            i += 1
+
         killer = tmpHarpies[i]
         del tmpHarpies[i]
         return killer
+
+class Coffee:
+    def __init__(self, frecuency, harpies):
+        self.frecuency = frecuency
+        self.lastTweet = ""
+        self.curretTweet = ""
+        self.harpies = harpies
+
+
+    def bang(self):
+
+        tmpHarpies=util.getSurvivors(self.harpies)
+        drinkers=self.chooseDrinkers(tmpHarpies)
+        tweet= "Coffee tweet"
+
+        //TODO tweet construction
+
+        return tweet
+
+    def chooseDrinkers(self, tmpHarpies):
+        drinkers=[]
+        nDrinkers= random.randint(2, 5)
+
+        random.shuffle(tmpHarpies)
+
+
+        for i in range(0, nDrinkers):
+            auxHarpy=random.choice(tmpHarpies)
+            tmpHarpies.remove(auxHarpy)
+            drinkers.append(auxHarpy)
+
+        return drinkers
